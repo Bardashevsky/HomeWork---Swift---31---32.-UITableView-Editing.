f2727ce8d078c988fc7c58c7cafdaa0d411cdcd7
Alex Bardashevsky <mijiekc@icloud.com>
2018-11-17 11:04:31 +0200

    Initial Commit

---
+++ HomeWork - Swift - 31 - 32. UITableView Editing./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork - Swift - 31 - 32. UITableView Editing.
+//
+//  Created by Oleksandr Bardashevskyi on 11/17/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
